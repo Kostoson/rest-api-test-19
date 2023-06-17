c4d7b0c3a4f2cb66b5bffb0b8ed382951a385966
Konstantin Dubinin <kostos1995@mail.ru>
2023-06-17 17:40:44 +0300

    add steps

---
+++ src/test/java/models/LoginResponseBody.java
@@ -0,0 +1,8 @@
+package models;
+
+import lombok.Data;
+@Data
+public class LoginResponseBody {
+    String token;
+            
+}
---
+++ src/test/java/models/SingleUserResponse.java
@@ -0,0 +1,13 @@
+package models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SingleUserResponse {
+    User data;
+
+}
